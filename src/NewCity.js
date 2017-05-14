9af7ad3e846cd5f0e1f479ffd0fd8596baf779c9
Dawid Adamek <adamdawidek@gmail.com>
2017-05-14 22:23:16 +0200

    Navigation to new view.

---
+++ src/NewCity.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class NewCity extends Component {
+  render() {
+    return (
+      <div className="NewCity">
+        <div className="container">
+          <div className="countries-group jumbotron">
+            elo
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewCity;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import NewCity from './NewCity';
 import './index.css';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Match} from 'react-router-dom'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Route path="/" component={App} />
+    <div>
+      <Route path="/" component={App} exact={true} />
+      <Route path="/new" component={NewCity} exact={true} />
+    </div>
   </BrowserRouter>,
   document.getElementById('root')
 );
