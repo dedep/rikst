1d4966a6460accc388c202f37f0dbcd9e0ec8e9e
Dawid Adamek <adamdawidek@gmail.com>
2017-05-14 21:43:41 +0200

    Introducing React router.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { BrowserRouter, Route } from 'react-router-dom'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <Route path="/" component={App} />
+  </BrowserRouter>,
   document.getElementById('root')
 );
