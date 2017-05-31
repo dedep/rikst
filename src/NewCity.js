6b71cc5af85c3e2e3fe71b0b88685d9346305611
Dawid Adamek <adamdawidek@gmail.com>
2017-05-31 22:36:52 +0200

    Styling new city form.

--- src/NewCity.js
+++ src/NewCity.js
@@ -8,7 +8,7 @@ import './NewCity.css';
 
 import {allCountries} from './repository/CountriesRepository';
 
-const GravatarValue = createClass({
+const CountryFlag = createClass({
   propTypes: {
     children: PropTypes.node,
     value: PropTypes.object
@@ -27,7 +27,6 @@ const GravatarValue = createClass({
 });
 
 var NewCity = createClass({
-
   getInitialState () {
     return {
       country: undefined
@@ -50,16 +49,34 @@ var NewCity = createClass({
     return (
       <div className="NewCity">
         <div className="container">
-          <div className="countries-group jumbotron">
-            <Select
-              name="form-field-name"
-              value={this.state.country}
-              searchable={true}
-              clearable={false}
-              options={options}
-              onChange={this.updateValue}
-              valueComponent={GravatarValue}
-            />
+          <div className="jumbotron">
+            <div className="row">
+              <div className="col-md-12 col-lg-6">
+                <Select
+                  name="form-field-name"
+                  value={this.state.country}
+                  searchable={true}
+                  clearable={false}
+                  options={options}
+                  onChange={this.updateValue}
+                  valueComponent={CountryFlag}
+                  placeholder="Wybierz państwo"
+                  noResultsText="Nie znaleziono państwa"
+                />
+              </div>
+            </div>
+
+            <div className="row city-input">
+              <div className="col-md-12 col-lg-6">
+                <div className="form-group">
+                  <input type="text" className="form-control" placeholder="Miasto" />
+                </div>
+              </div>
+            </div>
+
+            <div className="form-group submit">
+              <button type="button" className="col-md-3 col-sm-8 col-xs-12 btn btn-primary">ZAPISZ</button>
+            </div>
           </div>
         </div>
       </div>
