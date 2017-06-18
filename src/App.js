95a09d6fab19295a5eada9c3f5699a94e3da426d
Dawid Adamek <adamdawidek@gmail.com>
2017-06-18 23:04:02 +0200

    Serving cities from backend.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 import CityTile from './CityTile.js';
 import FloatingButton from './FloatingButton.js';
-import {cities} from './repository/CitiesRepository';
+
+import {countries} from './repository/CountriesRepository';
+import {City} from "./model/City";
 
 class App extends Component {
+
+  state = {users: []};
+
+  componentDidMount() {
+    fetch('/users')
+      .then(res => res.json())
+      .then(cts => {
+        let cities = cts.map(u => {
+          let country = countries[u.country];
+          return new City(country, u.city);
+        });
+
+        this.setState({users: cities});
+      });
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,7 +30,7 @@ class App extends Component {
           <div className="countries-group jumbotron">
             <div className="row">
               {
-                cities
+                this.state.users
                   .sort((a, b) => a.name.localeCompare(b.name))
                   .map(item => <CityTile key={item.name} country={item.country} city={item.name} />)
               }
