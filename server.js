0d9ded8bd055053570970c6af29c9ddf0eab1388
Dawid Adamek <adamdawidek@gmail.com>
2017-08-12 16:56:27 +0200

    Serving React files from Express.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = require('./src/app');
+const path = require('path');
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port);
+
+console.log(`Server listening on ${port}`);
